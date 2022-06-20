8a6ea80bb0e39c89537c4ec7ff1dbddd5a328845
Mmalagocki <mati0701@gmail.com>
2022-06-20 16:21:30 +0200

    All scraper with duplicates

--- webscrape/postscrape/spiders/quotes_spider.py
+++ webscrape/postscrape/spiders/quotes_spider.py
@@ -1,17 +1,24 @@
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
+from scrapy.shell import inspect_response
+
 
 class SuperSpider(CrawlSpider):
     name = 'spider'
     allowed_domains = ['quotes.toscrape.com']
     start_urls = ['http://quotes.toscrape.com/']
     base_url = 'http://quotes.toscrape.com'
-    rules = [Rule(LinkExtractor(allow = '/'),
-                  callback='parse_filter_book', follow=True)]
- 
-    def parse_filter_book(self, response):
-        for quote in response.css('div.quote'):
+
+    # allowed_domains = ['https://webscraper.io/test-sites/e-commerce/allinone']
+    # start_urls = ['https://webscraper.io/test-sites/e-commerce/allinone']
+    # base_url = 'https://webscraper.io/test-sites/e-commerce/allinone'
+
+    rules = [Rule(LinkExtractor(allow='/'),
+                  callback='parse', follow=True)]
+
+    def parse(self, response):
+        for quote in response.css('div'):
             yield {
-                'Link_without_base_url': quote.xpath('.//span/a/@href').get(),
-                'Text': quote.xpath('.//span[@class= "text"]/text()').get(),
+                'Link_without_base_url': quote.xpath('.//a/@href').get(),
+                'Text':  response.css("::text").extract(),
                 }
